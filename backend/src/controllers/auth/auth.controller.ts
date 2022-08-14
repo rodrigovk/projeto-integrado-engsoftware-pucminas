4211cffc90b65e1bf5135dab7810e01c14d89e5b
rodrigovk <rodrigocasadeialanis@hotmail.com>
2022-08-14 20:18:01 -0300

    Correções no basic auth (referente a async x await)

--- backend/src/controllers/auth/auth.controller.ts
+++ backend/src/controllers/auth/auth.controller.ts
@@ -7,14 +7,6 @@ export class AuthController {
   ) {}
 
   async handle(email: string, senha: string): Promise<void> {
-    // req.auth
-    // const email: string = req.body.email;
-    // const senha: string = req.body.senha;
-
-    // if (!(await this.authUseCase.execute(email))) {
-    //   throw new BadRequestException("Usuário não encontrado.");
-    // }
-
     await this.authUseCase.execute(email, senha);
 
     //? return res.status(204).send();
--- backend/src/frameworks/webserver/middlewares/basic-auth.ts
+++ backend/src/frameworks/webserver/middlewares/basic-auth.ts
@@ -4,45 +4,14 @@ import expressBasicAuth from 'express-basic-auth';
 
 export default function basicAuth(): RequestHandler {
   return expressBasicAuth({
-    authorizer: (username: any, password: any) => {
-      console.log("1")
+    authorizeAsync: true,
+    authorizer: async (username: any, password: any, callback: any) => {
       try {
-        authController.handle(username, password);
-        console.log("2")
-        return true;
+        await authController.handle(username, password);
+        return callback(null, true);
       } catch (err) {
-        console.log("3")
-        throw err;
+        throw err; //return callback(null, false);
       }
-      // const userMatches = expressBasicAuth.safeCompare(username, 'admin')
-      // const passwordMatches = expressBasicAuth.safeCompare(password, 'supersecret')
-      // return userMatches && passwordMatches
-    }
+    },
   })
 }
-
-// import { NextFunction, Response } from 'express';
-// import * as auth from 'express-basic-auth';
-
-// export default function basicAuth(req: auth.IBasicAuthedRequest, _res: Response, _next: NextFunction) {
-//   console.log('middleware: basic auth') //?
-
-//   //const user = auth(req); //? await
-//   console.log(req.auth);
-//   console.log(req.auth.user);
-//   console.log(req.auth.password);
-
-//   const username: string = 'test'
-//   const password: string = '123456'
-
-//   // if (user && user.name.toLowerCase() === username.toLowerCase() && user.password === password) {
-//   //     console.log('Basic Auth: success')
-//   //     next()
-//   // } else {
-//   //     console.log('Basic Auth: failure')
-//   //     res.statusCode = 401
-//   //     res.end('Access denied')
-//   // }
-// }
-
-// // export default basicAuth;
