75a5df0435945b40c62b8c16519068afce1318d5
rodrigovk <rodrigocasadeialanis@hotmail.com>
2022-09-09 15:43:32 -0300

    Inclusão de pacote de notificações

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from '@/router';
 import './tailwind.css';
 import VueNumberFormat from 'vue-number-format';
+import Notifications from 'notiwind';
 
 const pinia = createPinia();
 
@@ -15,4 +16,5 @@ const app = createApp(App)
   .use(pinia)
   .use(router)
   .use(VueNumberFormat, {prefix: 'R$ ', decimal: ',', thousand: '.'})
+  .use(Notifications)
   .mount('#app');
